f45c8c5f3fd0f29a4f75befb6c2794d6bf4b875b
hobenn-kim <sksjsksh32@naver.com>
2023-11-21 16:40:23 +0900

    first commit

---
+++ src/main/kotlin/com/practice/K6TestPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.practice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class K6TestPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<K6TestPracticeApplication>(*args)
+}
